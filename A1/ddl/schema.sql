efc1ec9cba01a5a046769332cec9010aa383d32a
Ahmet Hukul <ahukul@stud.hs-heilbronn.de>
2022-03-30 16:23:23 +0200

    Update schema.sql

--- A1/ddl/schema.sql
+++ A1/ddl/schema.sql
@@ -7,12 +7,6 @@ CREATE TABLE Patient(
     versicherungsnummer int
 );
 
-CREATE TABLE Station(
-    ID int PRIMARY KEY ,
-    bezeichnung varchar (15) NOT NULL,
-    bettenzahl int NOT NULL
-);
-
 CREATE TABLE Aufenthalt(
     ID int PRIMARY KEY ,
     P_ID int REFERENCES Patient(ID) ON DELETE RESTRICT,
@@ -24,6 +18,8 @@ CREATE TABLE Aufenthalt(
 CREATE TABLE Arzt(
     ID int PRIMARY KEY,
     V_ID int REFERENCES Arzt(ID) ON DELETE RESTRICT,
+	S_ID int REFERENCES Station(ID) ON DELETE RESTRICT,
+    AB_ID int REFERENCES Abteilung(ID) ON DELETE RESTRICT,
     vorname varchar (50) NOT NULL,
     nachname varchar (30) NOT NULL,
     geburtsdatum Date NOT NULL,
@@ -33,6 +29,7 @@ CREATE TABLE Arzt(
 CREATE TABLE Pflegekraft(
     ID int PRIMARY KEY,
 	PF_ID int REFERENCES Pflegekraft(ID) ON DELETE RESTRICT,
+	S_ID int REFERENCES Station(ID) ON DELETE RESTRICT,
     vorname varchar NOT NULL,
     nachname varchar NOT NULL,
     geburtsdatum Date NOT NULL
@@ -40,41 +37,47 @@ CREATE TABLE Pflegekraft(
 
 CREATE TABLE Abteilung(
     ID int PRIMARY KEY,
-    S_ID int REFERENCES Station(ID) ON DELETE RESTRICT,
-    A_ID int REFERENCES Arzt(ID) ON DELETE RESTRICT,
-    PF_ID int REFERENCES Pflegekraft(ID) ON DELETE RESTRICT,    
     bezeichnung varchar (15) NOT NULL
+);
+
+CREATE TABLE Station(
+    ID int PRIMARY KEY ,
+	AB_ID int REFERENCES Abteilung(ID) ON DELETE RESTRICT,
+    bezeichnung varchar (15) NOT NULL,
+    bettenzahl int NOT NULL CHECK (bettenzahl > 0)
 );
 
 CREATE TABLE Untersuchungsverfahren(
     ID int PRIMARY KEY,
+	U_ID int REFERENCES Untersuchungsverfahren(ID) ON DELETE RESTRICT,
     bezeichnung varchar (30) NOT NULL,
     koerperregion varchar (30) NOT NULL
 );
 
 CREATE TABLE Klinischesuntersuchungsverfahren(
     ID INT PRIMARY KEY,
+	FOREIGN KEY (ID) REFERENCES Untersuchungsverfahren(ID) ON DELETE CASCADE,
     bezeichnung varchar (30) NOT NULL,
-    koerperregion varchar (30) NOT NULL,
-	FOREIGN KEY (ID) REFERENCES Untersuchungsverfahren(ID)
+    koerperregion varchar (30) NOT NULL
 ) inherits(Untersuchungsverfahren);
 
 CREATE TABLE Apparativesuntersuchungsverfahren(
     ID INT PRIMARY KEY,
-    bezeichnung varchar (30) NOT NULL,
-	FOREIGN KEY (ID) REFERENCES Untersuchungsverfahren(ID)
+	FOREIGN KEY (ID) REFERENCES Untersuchungsverfahren(ID) ON DELETE CASCADE,
+    bezeichnung varchar (30) NOT NULL
 ) inherits(Untersuchungsverfahren);
 
 CREATE TABLE Labortest(
     ID INT PRIMARY KEY,
+	FOREIGN KEY (ID) REFERENCES Untersuchungsverfahren(ID) ON DELETE CASCADE,	
     probenart varchar (30) NOT NULL,
-    normwert int  CHECK (normwert <= 1.5 AND normwert >=0.5),
-	FOREIGN KEY (ID) REFERENCES Untersuchungsverfahren(ID)	
+    normwert int  CHECK (normwert <= 1.5 AND normwert >=0.5)
 ) inherits(Untersuchungsverfahren);
 
 CREATE TABLE Untersuchungsergebnis(
     ID int PRIMARY KEY,
-    UN_ID int REFERENCES Untersuchungsverfahren(ID) ON DELETE RESTRICT,
+    UN_ID int REFERENCES Untersuchungsverfahren(ID) ON DELETE CASCADE,
+	B_ID int REFERENCES Befund(ID) ON DELETE RESTRICT,
     ergebniszusammenfassung varchar,
     anforderungsdatum date NOT NULL,
     ergebnisdatum date
@@ -82,7 +85,6 @@ CREATE TABLE Untersuchungsergebnis(
 
 CREATE TABLE Diagnose(
     ID int PRIMARY KEY,
-    U_ID int REFERENCES Untersuchungsergebnis(ID) ON DELETE RESTRICT,
     icdcode int NOT NULL,
     diagnosetext varchar
 );
@@ -91,7 +93,11 @@ CREATE TABLE Befund(
     ID int PRIMARY KEY,
     A_ID int REFERENCES Arzt(ID) ON DELETE RESTRICT,
     P_ID int REFERENCES Patient(ID) ON DELETE RESTRICT,
-    D_ID int REFERENCES Diagnose(ID) ON DELETE RESTRICT,
     datum date NOT NULL,
     zusammenfassung varchar
+);
+
+CREATE TABLE Diagnoseliste(
+	B_ID int REFERENCES Befund(ID) ON DELETE RESTRICT,
+	D_ID int REFERENCES Diagnose(ID) ON DELETE RESTRICT
 )
