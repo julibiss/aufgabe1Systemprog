ccb1427b01c2a322f0ac8b40155ab8d96e0bc62a
Richard Zowalla <richard.zowalla@hs-heilbronn.de>
2022-04-04 13:27:33 +0200

    Adds A2 project template and related files for this exercise

---
+++ A2/src/main/java/de/hshn/mi/pdbg/basicservice/BasicDBServiceFactory.java
@@ -0,0 +1,24 @@
+package de.hshn.mi.pdbg.basicservice;
+
+
+/**
+ * {@link BasicDBServiceFactory} define a static factory method in order to create an instance of a
+ * {@link BasicDBService} object.
+ * @version 1.0
+ */
+public class BasicDBServiceFactory
+{
+	/**
+	 * Factory method in order to create a instance of a {@link BasicDBService} object.
+	 * @return instance of a {@link BasicDBService} object
+	 */
+	public static BasicDBService createBasicDBService()
+	{
+		//The code needed to instantiate an implementation of a BasicDBService. Use "jdbc:postgresql://postgres/pdbg-a2", "postgres", "postgres" for jdbc url, login and password in CI/CD context
+		throw new UnsupportedOperationException();
+    }
+}
+		
+			
+			
+		
