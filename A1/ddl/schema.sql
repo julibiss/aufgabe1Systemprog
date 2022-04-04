75fcd4104f43fd459330c6e75beac3d1060e5b01
Ahmet Hukul <ahukul@stud.hs-heilbronn.de>
2022-04-04 18:36:52 +0200

    Update schema.sql

--- A1/ddl/schema.sql
+++ A1/ddl/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE Department(
 
 CREATE TABLE Ward(
     ID int PRIMARY KEY ,
-	D_ID int REFERENCES Department(ID) ON DELETE RESTRICT,
+    D_ID int REFERENCES Department(ID) ON DELETE RESTRICT,
     name varchar (15) NOT NULL,
     numberOfBeds int NOT NULL CHECK (numberOfBeds > 0)
 );
@@ -29,9 +29,9 @@ CREATE TABLE HospitalStay(
 
 CREATE TABLE Physician(
     ID int PRIMARY KEY,
-    Supervisor int REFERENCES Physician(ID) ON DELETE RESTRICT,
-	W_ID int REFERENCES Ward(ID) ON DELETE RESTRICT,
-    D_ID int REFERENCES Department(ID) ON DELETE RESTRICT,
+    FOREIGN KEY Supervisor int REFERENCES Physician(ID) ON DELETE RESTRICT,
+    FOREIGN KEY W_ID int REFERENCES Ward(ID) ON DELETE RESTRICT,
+    FOREIGN KEY D_ID int REFERENCES Department(ID) ON DELETE RESTRICT,
     firstname varchar (50) NOT NULL,
     lastname varchar (30) NOT NULL,
     dateOfBirth Date NOT NULL,
@@ -40,56 +40,61 @@ CREATE TABLE Physician(
 
 CREATE TABLE Nurse(
     ID int PRIMARY KEY,
-	Supervisor int REFERENCES Nurse(ID) ON DELETE RESTRICT,
-	W_ID int REFERENCES Ward(ID) ON DELETE RESTRICT,
+    FOREIGN KEY Supervisor int REFERENCES Nurse(ID) ON DELETE RESTRICT,
+    FOREIGN KEY W_ID int REFERENCES Ward(ID) ON DELETE RESTRICT,
     firstname varchar NOT NULL,
     lastname varchar NOT NULL,
     dateOfBirth Date NOT NULL
 );
 
+
 CREATE TABLE Examination(
     ID int PRIMARY KEY,
-	E_ID int REFERENCES Examination(ID) ON DELETE RESTRICT,
+    FOREIGN KEY E_ID int REFERENCES Examination(ID) ON DELETE RESTRICT,
     name varchar (30) NOT NULL
 );
 
 CREATE TABLE ClinicalExamination(
     ID INT PRIMARY KEY,
-	FOREIGN KEY (ID) REFERENCES Examination(ID) ON DELETE CASCADE,
+    FOREIGN KEY (ID) REFERENCES Examination(ID) ON DELETE CASCADE,
     name varchar (30) NOT NULL,
     bodyPart varchar (30) NOT NULL
 ) inherits(Examination);
 
 CREATE TABLE TechnicalExamination(
     ID INT PRIMARY KEY,
-	FOREIGN KEY (ID) REFERENCES Examination(ID) ON DELETE CASCADE,
+    FOREIGN KEY (ID) REFERENCES Examination(ID) ON DELETE CASCADE,
     name varchar (30) NOT NULL
 ) inherits(Examination);
 
 CREATE TABLE LaboratoryTest(
     ID INT PRIMARY KEY,
-	FOREIGN KEY (ID) REFERENCES Examination(ID) ON DELETE CASCADE,	
+    FOREIGN KEY (ID) REFERENCES Examination(ID) ON DELETE CASCADE,  
     testName varchar (30) NOT NULL,
-	testType varchar(30) NOT NULL,
+    testType varchar(30) NOT NULL,
     standardValue int  CHECK (standardValue <= 1.5 AND standardValue >=0.5)
 ) inherits(Examination);
 
 CREATE TABLE Finding(
     ID int PRIMARY KEY,
-    PH_ID int REFERENCES Physician(ID) ON DELETE RESTRICT,
-    P_ID int REFERENCES Patient(ID) ON DELETE RESTRICT,
+    FOREIGN KEY PH_ID int REFERENCES Physician(ID) ON DELETE RESTRICT,
+    FOREIGN KEY P_ID int REFERENCES Patient(ID) ON DELETE RESTRICT,
     dateOfFinding date NOT NULL,
     summary varchar NOT NULL
 );
 
 CREATE TABLE ExaminationResult(
     ID int PRIMARY KEY,
-    E_ID int REFERENCES Examination(ID) ON DELETE CASCADE,
-	F_ID int REFERENCES Finding(ID) ON DELETE RESTRICT,
+    FOREIGN KEY E_ID int REFERENCES Examination(ID) ON DELETE CASCADE,
+    FOREIGN KEY F_ID int REFERENCES Finding(ID) ON DELETE RESTRICT,
     resultSummary varchar,
     requestDate date NOT NULL,
     resultDate date CHECK (resultDate > requestDate)
 );
+
+
+
+
 
 CREATE TABLE Diagnosis(
     ID int PRIMARY KEY,
@@ -98,7 +103,7 @@ CREATE TABLE Diagnosis(
 );
 
 CREATE TABLE Diagnosis_Finding(
-	F_ID int REFERENCES Finding(ID) ON DELETE RESTRICT,
-	D_ID int REFERENCES Diagnosis(ID) ON DELETE RESTRICT,
+    F_ID int REFERENCES Finding(ID) ON DELETE RESTRICT,
+    D_ID int REFERENCES Diagnosis(ID) ON DELETE RESTRICT,
     PRIMARY KEY(F_ID,D_ID)
 )
