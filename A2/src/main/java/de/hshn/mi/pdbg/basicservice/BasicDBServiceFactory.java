52a037ffddb507897aca806006f1d32154b8a8d8
ahmet <ahukul@stud.hs-heilbronn.de>
2022-05-05 15:32:43 +0200

    Checkstyle angepasst, wie ein Profi                                                       😎

--- A2/src/main/java/de/hshn/mi/pdbg/basicservice/BasicDBServiceFactory.java
+++ A2/src/main/java/de/hshn/mi/pdbg/basicservice/BasicDBServiceFactory.java
@@ -14,7 +14,7 @@ public class BasicDBServiceFactory {
      * @return instance of a {@link BasicDBService} object
      */
     public static BasicDBService createBasicDBService() {
-        return new BasicDBServiceImpl("jdbc:postgresql://localhost:5432/postgres",
-                "postgres", "password");
+        return new BasicDBServiceImpl(" jdbc:postgresql://postgres/pdbg−a2",
+                "postgres", "postgres");
     }
 }
