ebf24e3c7f333ee42b437ae6f6e3f09228171b13
ahmet <ahukul@stud.hs-heilbronn.de>
2022-06-09 17:22:44 +0200

    7 / 7 Test passed a4a CMON PIPELINE

--- A4A/src/main/java/de/hshn/mi/pdbg/basicservice/DBCreator.java
+++ A4A/src/main/java/de/hshn/mi/pdbg/basicservice/DBCreator.java
@@ -14,7 +14,7 @@ public class DBCreator implements SchemaGenerator {
 
 
     public static void main(String[] args) throws Exception {
-        String url = "jdbc:postgresql://postgres/pdbg-baseline";
+        String url = "jdbc:postgresql://postgres/pdbg-a2";
         new DBCreator().createDatabase(url, "postgres", "postgres");
     }
 
