91a77bb0ba9fe74fad85b771091ca3005e7dd0e5
ahmet <ahukul@stud.hs-heilbronn.de>
2022-06-10 12:09:56 +0200

    connection to db wont work hope this fixes it part 3

--- A4A/src/main/java/de/hshn/mi/pdbg/basicservice/DBGeneratorSystemUnderTest.java
+++ A4A/src/main/java/de/hshn/mi/pdbg/basicservice/DBGeneratorSystemUnderTest.java
@@ -5,6 +5,7 @@ package de.hshn.mi.pdbg.basicservice;
  */
 public class DBGeneratorSystemUnderTest {
     public static void main(String[] args) throws Exception {
+        BaselineDBServiceFactory.createBaselineDBService();
         new de.hshn.mi.pdbg.basicservice.dbgen.DBGenerator(
                 SystemUnderTestDBServiceFactory.createSystemUnderTestDBService()).generateDB(100);
     }
