6ae39be6795b7eaab6381bb0fc073e028d0cdb48
ahmet <ahukul@stud.hs-heilbronn.de>
2022-06-10 23:54:04 +0200

    connection to db wont work hope this fixes it part 10

--- A4A/src/main/java/de/hshn/mi/pdbg/basicservice/SystemUnderTestDBServiceFactory.java
+++ A4A/src/main/java/de/hshn/mi/pdbg/basicservice/SystemUnderTestDBServiceFactory.java
@@ -15,7 +15,7 @@ public class SystemUnderTestDBServiceFactory {
      */
     public static BasicDBService createSystemUnderTestDBService() {
         try {
-            return new BasicDBServiceImpl("jdbc:postgresql://postgres/pdbg-a2",
+            return new BasicDBServiceImpl("jdbc:postgresql://postgres/pdbg-a4a",
                     "postgres", "postgres");
         } catch (SQLException e) {
             throw new IllegalArgumentException(e.getMessage());
